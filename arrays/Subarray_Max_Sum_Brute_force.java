191569ab1c96f159cb8d018b205dc3091bcf5b90
Barirah <barirahsundus76@gmail.com>
2026-06-26 13:33:48 +0530

    Initial commit

---
+++ arrays/Subarray_Max_Sum_Brute_force.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Subarray_Max_Sum_Brute_force
+{
+    public static void MaxSum(int num[])
+    {
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < num.length; i++)
+        {
+            for(int j = i; j < num.length; j++)  
+            {
+                int sum = 0;
+                for(int k = i; k <= j; k++)      
+                {
+                    sum = sum + num[k];
+                }
+                if(sum >= max)
+                {
+                    max = sum;
+                }
+            }
+        }
+        System.out.println("maximum sum is: " + max);
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        int num[] = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            num[i] = sc.nextInt();
+        }
+        MaxSum(num);
+    }
+}
